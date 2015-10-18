bdecb3c778f2b87d7747c0d45c9ec616183c6296
Riccardo Macoratti <kimahri_san@zoho.com>
2015-10-18 23:43:24 +0200

    Added use of paramiko and scpclient for trasmmitting configs and starting services on the clients. Getpass is used when asking passwords.
    Menu has 3 voices, first generates configs, second upload configs and services,
    third cleans locally created config.
    
    In ``gen_config``, now config generation works out of the box, with all matrix
    setups. A json at the end is written to save changes for next sessions.
    
    In ``upload_all``, informations for every tile are asked, then config and
    service files are uploaded.

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # This is part of PiMCPlayer.
@@ -20,11 +20,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
-import sys, string, random
+import os, sys, string, json, random
 from os import path
-from collections import deque
+from contextlib import closing
 
 import paramiko as pm
+import scpclient
+from getpass import getpass
 
 ################################################################################
 # METADATA                                                                     #
@@ -34,15 +36,41 @@ VERSION = '1.0.0'
 ################################################################################
 
 LOCATION = path.dirname(sys.argv[0])
+SAVEFILE = path.join(LOCATION, 'save.json')
+USERNAME = 'pi'
+PASSWORD = 'raspberry'
 
 # already used random strings
 rand_used = set()
 # queue for menu printing
-menu = deque()
+menu = []
 # information dict
-info = { 'monitors': [] }
+info = None
+
+def error(string):
+  '''
+  Print an error string like:
+  '[!] message'
+  
+  < string: error message
+  '''
+  print('[!] {}'.format(string), file=sys.stderr)
+
+def note(string):
+  '''
+  Print a warning string like:
+  '[*] message'
+  
+  < string: error message
+  '''
+  print('[*] {}'.format(string))
 
 def rand_name():
+  '''
+  Returns a random 8 alphabetic characters string.
+  
+  > rand: random string 
+  '''
   while True:
     rand = ''.join(random.choice(string.ascii_letters) for i in range(8))
     if rand not in rand_used:
@@ -51,7 +79,7 @@ def rand_name():
   
   return rand
 
-def question(message, type_='str', check=None):
+def question(message, type_='str', check=lambda e: True, password=False):
   '''
   Prints a question and continues to ask until input is correct.
   
@@ -68,9 +96,12 @@ def question(message, type_='str', check=None):
   while True:
     # if it's not the first time asking
     if sel is not None:
-      print('Input provided is not valid. Please, retry.', file=sys.stderr)
+      error('Input provided is not valid. Please, retry.')
     try:
-      sel = etype(input('[Q] ' + message + ' '))
+      if password:
+        sel = etype(getpass('[?] {} '.format(message)))
+      else:
+        sel = etype(input('[?] {} '.format(message)))
       if check(sel):
         break
     except ValueError:
@@ -80,6 +111,10 @@ def question(message, type_='str', check=None):
   return sel
       
 def print_menu():
+  '''
+  Prints menu option, waiting for user selection, and reasking question if
+  inserted data is not valid.
+  '''
   count = 1
   for entry in menu:
     print('{}) {}'.format(count, entry))
@@ -99,7 +134,31 @@ def print_menu():
   
   return number
 
+def clean_up(message=False):
+  '''
+  Clean previously generated config files, plus save file.
+  
+  < message: notify to user of deleted files.
+  '''
+  config = path.join(LOCATION, 'config')
+  for file in os.listdir(config):
+    if file.startswith('pi'):
+      filepath = path.join(config, file)
+      os.remove(filepath)
+      if message:
+        note('{} removed.'.format(filepath))
+  filepath = path.join(LOCATION, 'save.json')
+  if path.isfile(filepath):
+    os.remove(filepath)
+
 def gen_config():
+  # clean up before starting
+  clean_up()
+  
+  # init dict information
+  info = {}
+  
+  # begin to gather information from user
   wallpath = path.join(LOCATION, 'config', 'piwall')
   with open(wallpath, 'w') as wall:
     info['wall'] = rand_name()
@@ -151,53 +210,20 @@ def gen_config():
     # width in millimeters.
     info['bezel'] = round(info['width'] / info['mm_width'] * mm_bezel)
     
-    # determine if wall is a vector
-    vert_vect = False
-    horiz_vect = False
-    if info['arr_col'] == 1:
-      vert_vect = True
-    if info['arr_row'] == 1:
-      horiz_vect = True
+    info['monitors'] = []
     count = 1
     for i in range(info['arr_row']):
-      y = i * info['height']
+      x = 0 if i == 0 else (i * info['width']) + info['bezel']
       for k in range(info['arr_col']):
-        x = k * info['width']
+        y = 0 if k == 0 else (k * info['height']) + info['bezel']
         monitor = {
           'id': count,
           'name': rand_name(),
+          'width': info['width'],
+          'height': info['height'],
           'x': x,
           'y': y
         }
-        # if 0,0;0,N;N,0;N,N
-        if (k == 0 or k == info['arr_col']-1) \
-          and (i == 0 or i == info['arr_row']-1):
-          if vert_vect:
-            monitor['width'] = info['width']
-          else:
-            monitor['width'] = info['width'] - info['bezel']
-          if horiz_vect:
-            monitor['height'] = info['height']
-          else:
-            monitor['height'] = info['height'] - info['bezel']
-        # if x,0/N
-        elif k == 0 or k == info['arr_col']-1:
-          if vert_vect:
-            monitor['width'] = info['width']
-          else:
-            monitor['width'] = info['width'] - info['bezel']
-          monitor['height'] = info['height'] - (2 * info['bezel'])
-        # if 0/N,x
-        elif i == 0 or i == info['arr_row']-1:
-          monitor['width'] = info['width'] - (2 * info['bezel'])
-          if horiz_vect:
-            monitor['height'] = info['height']
-          else:
-            monitor['height'] = info['height'] - info['bezel']
-        # if x,x
-        else:
-          monitor['width'] = info['width'] - (2 * info['bezel'])
-          monitor['height'] = info['height'] - (2 * info['bezel'])
         info['monitors'].append(monitor)
         count += 1
     
@@ -220,34 +246,154 @@ def gen_config():
     wall.write('[pimcplayer]\n')
     for mon in info['monitors']:
       wall.write('pi{}={}\n'.format(mon['id'], mon['name']))
+    
+    # save info into a json file
+    with open(SAVEFILE, 'w') as js:
+      json.dump(info, js, indent=4)
 
-def upload_config():
-  #ssh = pm.SSHClient()
-  #ssh.set_missing_host_key_policy(pm.AutoAddPolicy())
-  #ssh.connect('192.168.206.80', username='pi', password='raspberry')
-  #
-  #with 
+def _fixed_scp_read_response(channel):
+  '''
+  Corrects library method in evaluating response byte.
+  
+  < channel: channel to init scp communication
+  '''
+  scpclient.msg = scpclient._scp_recv(channel, scpclient._MISC_BUF_LEN)
 
-def upload_service():
-  pass
+  if not scpclient.msg:
+    raise scpclient.SCPError('Empty response')
+
+  if scpclient.msg[0] == 0:
+    # Normal result.
+    return
+  elif scpclient.msg[0] == 1:
+    raise scpclient.SCPError('Server error: {0!r}'.format(
+      scpclient.msg[1:]))
+  else:
+    raise scpclient.SCPError('Invalid response: {0!r}'.format(
+      scpclient.msg))
+
+def ssh_connect(hostname, username, password):
+  '''
+  Create a ssh connection to host, with username and password given.
+  
+  < hostname: string host or ip to connect
+  < username: username for connection
+  < password: password for connection
+  
+  > ssh: paramiko ssh connection object
+  '''
+  ssh = pm.SSHClient()
+  ssh.set_missing_host_key_policy(pm.AutoAddPolicy())
+  ssh.connect(hostname=hostname, username=username, password=password)
+  
+  return ssh
+
+def ssh_command(command, ssh, password, root=False, no_out=False):
+  '''
+  Launch a command from ssh client.
+
+  < command: string command to execute
+  < ssh: paramiko ssh client already connected
+  < password: optional overwrite password
+  < root: flag, true if command is to execute as root
+  < no_out: suppress output
+  '''
+  # force sudo to ask password and take it from stdin
+  if root:
+    command = 'sudo -k -S {}'.format(command)
+
+  stdin, stdout, stderr = ssh.exec_command(command)
+  if root:
+    # give the password
+    stdin.write('{}\n'.format(password))
+    stdin.flush()
+  if not no_out:
+    for line in stdout:
+      print(line, end='')
+
+def ssh_file(filename, ssh, out_name=None, permissions='0644'):
+  '''
+  Send file to host marked by ssh connection.
+  
+  < filename: path of file to send
+  < ssh: paramiko ssh connection object
+  < out_name: name to give to sent file
+  < permission: octal representation of permissions
+  '''
+  if not out_name:
+    out_name = path.basename(filename)
+  with closing(scpclient.Write(ssh.get_transport(), '.')) as scp:
+    scp.send_file(filename, override_mode=permissions, remote_filename=out_name)
+
+def upload_all():
+  '''
+  Upload configuration (.piwall, .pitile) to the tiles.
+  '''
+  # if not step 1 do not execute step 2
+  if not path.isfile(SAVEFILE):
+    error('Generate config before uploading it!')
+    return
+  
+  # if info informations aren't already loaded, load them from json file
+  global info
+  if not info:
+    with open(SAVEFILE, 'r') as js:
+      info = json.load(js)
+  
+  # inject code into library
+  scpclient._scp_read_response = _fixed_scp_read_response
+  
+  piwall = path.join(LOCATION, 'config', 'piwall')
+  pitile = path.join(LOCATION, 'config', 'pitile')
+  pwomxp_serv = path.join(LOCATION, 'scripts', 'pwomxplayer')
+  pwomxp_conf = path.join(LOCATION, 'config', 'pwomxplayer')
+  for dev in sorted(info['monitors'], key=lambda x: x['id']):
+    # gather hostname and eventual password for the tile
+    print('Tile {}:'.format(dev['id']))
+    hostname = question(
+      'Hostname or address?',
+      check=lambda e: len(e) > 0
+    )
+    user = question(
+      'Username? (default "{}")'.format(USERNAME)
+    )
+    if len(user) == 0: user = USERNAME
+    password = question(
+      'Password for user "{}"? (default "{}")'.format(user, PASSWORD),
+      password=True
+    )
+    if len(password) == 0: password = PASSWORD
+    
+    # upload piwall and pitile files
+    ssh = ssh_connect(hostname, user, password)
+    ssh_file(piwall, ssh, '.piwall')
+    ssh_file('{}{}'.format(pitile, dev['id']), ssh, out_name='.pitile')
+    
+    # upload service files
+    ssh_file(pwomxp_conf, ssh, permissions='0755')
+    ssh_command('mv pwomxplayer /etc/default', ssh, password, True, True)
+    ssh_file(pwomxp_serv, ssh, permissions='0755')
+    ssh_command('mv pwomxplayer /etc/init.d', ssh, password, True, True)
+    ssh_command('update-rc.d pwomxplayer defaults', ssh, password, True, True)
+    ssh_command('/etc/init.d/pwomxplayer start', ssh, password, True, True)
 
 def main():
   while True:
-    if len(menu) != 0:
+    if len(menu) == 0:
+      menu.append('Generate configuration files')
+      menu.append('Install configuration and player service')
+      menu.append('Clean created configuration files')
+    else:
       print()
-    
-    menu.append('Generate configuration files')
-    menu.append('Install configuration files')
-    menu.append('Install player service')
 
     number = print_menu()
 
     if number == 1:
       gen_config()
     elif number == 2:
-      upload_config()
-    else:
-      upload_service()
+      upload_all()
+    elif number == 3:
+      clean_up(True)
 
 if __name__ == '__main__':
   main()
